7bb4bb6c1438b9059a9dc2ac93a41034e22d317d
Ruslan Mandell <78404468+RuslanAMandell@users.noreply.github.com>
2024-12-02 21:32:15 -0500

    Update main.c

--- examples/main.c
+++ examples/main.c
@@ -42,7 +42,7 @@ int main() {
     }
 
     float prediction = predict_linear_regression(new_data_matrix, weights, 0);
-    printf("\nPrediction for new data point [4.0, 5.0]: %f\n", prediction);
+    printf("\nPrediction for new data point [%f, %f]: %f\n",new_data[0],new_data[1], prediction);
 
     // Free allocated memory
     free_matrix(X);
